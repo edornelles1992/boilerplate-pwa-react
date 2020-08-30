2534db8fae661431638f931b991a1dd8f6c4add0
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-08-30 05:58:37 -0300

    eject e ajuste para suporte dos deocorators no projet

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [ require('@babel/plugin-proposal-decorators').default,
-        {
-           legacy: true
-        }],
+    plugins: [
+        ["@babel/plugin-transform-flow-strip-types"],
+        ["@babel/plugin-proposal-decorators", { "legacy": true}],
+        ["@babel/plugin-proposal-class-properties", { "loose": true}]
+    ]
   };
